d69df26cca21ceb25cc3bcf7b8b495b3e52a2cad
Letitiac33 <157053515+Letitiac33@users.noreply.github.com>
2024-10-08 11:16:31 -0400

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,13 +1,20 @@
 from typing import Any, Optional
 
-age: Optional[int] = None
-animal_id: int
-health_status: Optional[str] = None
-species: str
-start_date: str
-start_location: Habitat
-status: str = "Scheduled"
-
 class Animal:
+    def __init__(self,
+                 animal_id: int,
+                 species: str,
+                 age: Optional[int] = None,
+                 health_status: Optional[str] = None,
+                 start_date: str,
+                 start_location: Habitat,
+                 status: str = "Scheduled") -> None:
+        self.animal_id = animal_id
+        self.species = species
+        self.age = age
+        self.health_status = health_status
+        self.start_date = start_date
+        self.start_location = start_location
+        self.status = status
 
     pass

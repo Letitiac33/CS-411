951ead63f645fed0e50930dc9f6f3343867eac94
Letitiac33 <157053515+Letitiac33@users.noreply.github.com>
2024-10-08 12:06:01 -0400

    Update migration_path.py

--- migration_path.py
+++ migration_path.py
@@ -1,6 +1,16 @@
 from typing import Optional
 
 class MigrationPath:
+    def __init__(self,
+                 destination: Habitat,
+                 duration: Optional[int] = None,
+                 migration_path: 'MigrationPath' = None, 
+                 path_id: int = 0,
+        self.destination = destination
+        self.duration = duration
+        self.migration_path = migration_path
+        self.migrations = migrations or {}
+        self.path_id = path_id
     def get_migration_path_details(path_id) -> dict:
         pass
     def update_migration_path_details(path_id: int, **kwargs) -> None:

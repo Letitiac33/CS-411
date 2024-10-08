fddf85eaced84db71c81369df3b885c117b5b6a9
Letitiac33 <157053515+Letitiac33@users.noreply.github.com>
2024-10-08 11:10:52 -0400

    Update migration.py

--- migration.py
+++ migration.py
@@ -4,6 +4,11 @@ current_date: str
 current_location: str
 destination: Habitat
 duration: Optional[int] = None
+migration_id: int
+migration_path: MigrationPath
+migrations: dict[int, Migration] = {}
+path_id: int
+paths: dict[int, MigrationPath] = {}
 
 class Migration:
 

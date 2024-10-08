f8671bcae6b48676fd85abaa4cab1b59d9f81e0f
Letitiac33 <157053515+Letitiac33@users.noreply.github.com>
2024-10-08 12:03:45 -0400

    Update migration_path.py

--- migration_path.py
+++ migration_path.py
@@ -3,3 +3,5 @@ from typing import Optional
 class MigrationPath:
     def get_migration_path_details(path_id) -> dict:
         pass
+    def update_migration_path_details(path_id: int, **kwargs) -> None:
+        pass

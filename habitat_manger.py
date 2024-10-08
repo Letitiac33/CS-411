972428e9899aae0642bde8ee7ced2797a68f9077
Letitiac33 <157053515+Letitiac33@users.noreply.github.com>
2024-10-08 11:38:03 -0400

    Update habitat_manger.py

--- habitat_manger.py
+++ habitat_manger.py
@@ -4,7 +4,7 @@ from wildlife_tracker.habitat_management.habitat import Habitat
 class HabitatManager:   
     def __init__(self) -> None:
         habitats: dict[int, Habitat] = {}
-        pass
+
      def create_habitat(self, habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
         pass
 

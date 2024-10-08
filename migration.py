c665398d5362a0c47326ef714aeae603a5fa8dc2
Letitiac33 <157053515+Letitiac33@users.noreply.github.com>
2024-10-08 12:06:30 -0400

    Update migration.py

--- migration.py
+++ migration.py
@@ -5,11 +5,7 @@ class Migration:
                  migration_id: int,
                  current_date: str,
                  current_location: str,
-                 destination: Habitat,
-                 duration: Optional[int] = None,
-                 migration_path: 'MigrationPath' = None, 
                  migrations: Optional[Dict[int, 'Migration']] = None,
-                 path_id: int = 0,
                  paths: Optional[Dict[int, 'MigrationPath']] = None,
                  species: str = "",
                  animals: Optional[Dict[int, Animal]] = None) -> None:
@@ -17,11 +13,7 @@ class Migration:
         self.migration_id = migration_id
         self.current_date = current_date
         self.current_location = current_location
-        self.destination = destination
-        self.duration = duration
-        self.migration_path = migration_path
         self.migrations = migrations or {}
-        self.path_id = path_id
         self.paths = paths or {}
         self.species = species
         self.animals = animals or {}

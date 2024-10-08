0c26db270e6bf8bd8e6be6716fcfbc1ebfc0e9be
Letitiac33 <157053515+Letitiac33@users.noreply.github.com>
2024-10-08 12:02:50 -0400

    Update migration_manager.py

--- migration_manager.py
+++ migration_manager.py
@@ -4,13 +4,16 @@ from wildlife_tracker.migration_tracking.migration_path import Migration_Path
 
 
 class MigrationManager:
+    def __init__(self) -> None:
+        habitats: dict[int, Habitat] = {}
+        
     def create_migration_path(species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
         pass
 
-    def get_migration_by_id(migration_id: int) -> Migration:
+    def get_migration_by_id(self) -> Migration:
         pass
     
-    def get_migration_path_by_id(path_id: int) -> MigrationPath:
+    def get_migration_path_by_id(self) -> MigrationPath:
         pass
     
     def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
@@ -25,7 +28,7 @@ class MigrationManager:
     def get_migrations_by_current_location(current_location: str) -> list[Migration]:
         pass
     
-    def get_migrations_by_migration_path(migration_path_id: int) -> list[Migration]:
+    def get_migrations_by_migration_path(self) -> list[Migration]:
         pass
     
     def get_migrations_by_start_date(start_date: str) -> list[Migration]:
@@ -34,10 +37,7 @@ class MigrationManager:
     def get_migrations_by_status(status: str) -> list[Migration]:
         pass
     
-    def get_migration_path_details(path_id) -> dict:
-        pass
-    
-    def remove_migration_path(path_id: int) -> None:
+    def remove_migration_path(self) -> None:
         pass
     
     def schedule_migration(migration_path: MigrationPath) -> None:
